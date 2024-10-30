fbac2a721a6ff8fb949f79854fd774975f454ea0
Brendan Dasilva <brendan.dasilva@gmail.com>
2024-10-30 10:25:24 -0400

    Project initialization

---
+++ src/main/java/ca/gbc/gbc_eventbooking_group33/GbcEventBookingGroup33Application.java
@@ -0,0 +1,13 @@
+package ca.gbc.gbc_eventbooking_group33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcEventBookingGroup33Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GbcEventBookingGroup33Application.class, args);
+  }
+
+}
