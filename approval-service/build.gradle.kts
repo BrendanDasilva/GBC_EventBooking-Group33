af632287d7543e676dd64d5c02959f7f31acb477
Kailie Field <kailie.field@gmail.com>
2024-11-07 03:12:13 -0500

    running: approval-service, booking-service, event-service, databases also functioning. Not functioning: room-service, user-service not tested yet

--- approval-service/build.gradle.kts
+++ approval-service/build.gradle.kts
@@ -25,10 +25,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
+//	implementation("org.flywaydb:flyway-core")
+//	implementation("org.flywaydb:flyway-database-postgresql")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
