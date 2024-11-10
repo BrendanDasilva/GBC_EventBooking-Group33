e18c58054ac0a22ac2ec4a52f00130bc051bece9
BrendanDasilva <124002477+BrendanDasilva@users.noreply.github.com>
2024-11-09 20:38:34 -0500

    Delete room-service/build directory

--- room-service/build/resources/main/db/migration/V1__init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rooms (
-    id SERIAL PRIMARY KEY,
-    room_name VARCHAR(100) NOT NULL,
-    capacity INTEGER NOT NULL,
-    features TEXT,
-    available BOOLEAN NOT NULL DEFAULT TRUE
-    );
